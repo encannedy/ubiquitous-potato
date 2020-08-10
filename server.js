a8bd7b4cf55f590f00123ea4f0636b4eb5e4fbf4
Erica Cannedy <encannedy@gmail.com>
2020-08-09 22:07:12 -0400

    attempt fix to react app

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
-const bodyParser =  require("body-parser");
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
-app.use(bodyParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -19,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes)
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1.27017/googlebooks", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
 //may need to change mongodb://127.0.0.1.27017/googlebooks to mongodb://localhost/googlebooks
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
