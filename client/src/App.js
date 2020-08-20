ccdd1579edb4b932fe1282822311e01a23e4f935
Erica Cannedy <encannedy@gmail.com>
2020-08-20 18:23:59 -0400

    heroku1

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Header from "./components/Header";
-import Wrapper from "./components/Wrapper";
+import NavBar from "./components/NavBar/index";
+import Header from "./components/Header/index";
+import Wrapper from "./components/Wrapper/index";
 
 import Search from "./pages/search";
 import Saved from "./pages/saved";
