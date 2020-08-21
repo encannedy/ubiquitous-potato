0608a2f1f22ba76900c58d2207158f981c5b1e13
Erica Cannedy <encannedy@gmail.com>
2020-08-20 21:31:20 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -4,29 +4,26 @@ import NavBar from "./components/NavBar/index";
 import Header from "./components/Header/index";
 import Wrapper from "./components/Wrapper/index";
 
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
+//import Search from "./pages/Search";
+//import Saved from "./pages/Saved";
 
 import "./App.css";
 
-class App extends Component {
- 
-  render() {
+function App() {
+
     return (
       <Router>
         <div>
           <NavBar />
           <Header />
           <Wrapper>
-            <Route exact path="/" component={Search} />
-            <Route exact path="/search" component={Search} />
-            <Route exact path="/saved" component={Saved} />
+
+
 
           </Wrapper>
         </div>
       </Router>
     )
   };
-};
 
 export default App;
