3afd47c66ed50b20e70be9d1ce7fb257df6fb480
Erica Cannedy <encannedy@gmail.com>
2020-08-20 21:55:56 -0400

    updates

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes1");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
